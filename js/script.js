b9ac4c04f78f8d01d399b7281a4978e421fc8dca
Aleksandr <faceless178@yandex.ru>
2022-07-28 22:01:46 +0300

    Final stage

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
+const numberEl = document.querySelector('.number')
+let number = 0;
+let timerId;
+
+const btnStart = document.querySelector('.start');
+btnStart.onclick = function() {
+    timerId = setInterval(function() {
+        number++;
+        numberEl.innerText = number;
+    }, 1000);
+}
+
+const btnStop = document.querySelector('.stop');
+btnStop.onclick = function() {
+    clearInterval(timerId);
+}
+
+const btnReset = document.querySelector('.reset');
+btnReset.onclick = function() {
+    number = 0;
+    numberEl.innerText = 0;
+    clearInterval(timerId);
+}
+
 
